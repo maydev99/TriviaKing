9881dcdedae1711d07adbbf3da2a90e6a3f4ef7c
michael <maydev99@gmail.com>
2019-10-24 19:09:33 -0400

    Added UI Parsed json data to show question and answers in list with checking code to determine is answer is correct

--- app/src/main/java/com/bombadu/triviaking/MainActivity.kt
+++ app/src/main/java/com/bombadu/triviaking/MainActivity.kt
@@ -1,22 +1,27 @@
 package com.bombadu.triviaking
 
+import android.content.pm.ActivityInfo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
+import android.widget.*
+import androidx.appcompat.app.AlertDialog
 import kotlinx.android.synthetic.main.activity_main.*
 import okhttp3.*
 import org.json.JSONException
+import org.json.JSONObject
 import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
 
     private val client = OkHttpClient()
+    var isCorrect = false
+    var correctAnswer = ""
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var textView = findViewById<TextView>(R.id.helloworldTV);
+        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
 
         fetchData()
     }
@@ -32,23 +37,75 @@ class MainActivity : AppCompatActivity() {
             }
 
             override fun onResponse(call: Call, response: Response) {
+                var question = ""
+
+                var incorrectList = mutableListOf<String>()
+
+
 
                 try {
-                    myResponse = response.body!!.string()
-                    println(myResponse)
+                    val myRepsonse = response.body!!.string()
+                    println(myRepsonse)
+
+                    val jsonObject = JSONObject(myRepsonse)
+                    val resultsJa = jsonObject.getJSONArray("results")
+                    for (i in 0 until resultsJa.length()) {
+                        val jsonIndex = resultsJa.getJSONObject(i)
+                        question = jsonIndex.getString("question")
+                        correctAnswer = jsonIndex.getString("correct_answer")
+
+                        val incorrectAnswersJA = jsonIndex.getJSONArray("incorrect_answers")
+                        for (j in 0 until incorrectAnswersJA.length()) {
+                            var incorrectAnswer = incorrectAnswersJA.getString(j)
+                           // var incorrectAnswer: String = incorrectAnswersJA[i] as String
+                           incorrectList.add(incorrectAnswer.toString())
+
+                        }
+
+
+
+
+                    }
+
                 }catch (e: JSONException) {
                     e.printStackTrace()
                 }
 
                 if (response.isSuccessful) runOnUiThread {
-                    helloworldTV.text = myResponse
+                    questionTextView.text = question
+                    incorrectList.add(correctAnswer)
+                    incorrectList.shuffle()
+                    var answerListView = findViewById<ListView>(R.id.answerListView)
+                    var myAdapter = ArrayAdapter<String> (this@MainActivity, android.R.layout.simple_list_item_1, incorrectList)
+                    answerListView.adapter = myAdapter
+                    answerListView.onItemClickListener = AdapterView.OnItemClickListener { parent, view, position, id ->
+                        var selectedAnswer = answerListView.getItemAtPosition(position)
+                        isCorrect = selectedAnswer.equals(correctAnswer)
+                        showResultDialog()
+
+                    }
                 }
-
-
             }
-
-
         })
+
+    }
+
+    private fun showResultDialog() {
+        val dialog = AlertDialog.Builder(this)
+        if(isCorrect){
+            dialog.setTitle("Correct")
+            dialog.setMessage("Good Job!")
+        } else {
+            dialog.setTitle("Incorrect")
+            dialog.setMessage("The correct answer is $correctAnswer")
+        }
+
+        dialog.setPositiveButton("Continue") {dialog, which ->
+            fetchData()
+
+        }
+
+        dialog.show()
 
     }
 }
