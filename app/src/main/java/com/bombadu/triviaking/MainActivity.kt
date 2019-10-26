e8a0cf3bc269fbc5b5dfacc3c7ed957d6c26821a
michaelmay <may@droidlot.com>
2019-10-26 10:07:11 -0400

    Added Scoring

--- app/src/main/java/com/bombadu/triviaking/MainActivity.kt
+++ app/src/main/java/com/bombadu/triviaking/MainActivity.kt
@@ -1,13 +1,15 @@
 package com.bombadu.triviaking
 
 import android.content.pm.ActivityInfo
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.*
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import android.widget.TextView
 import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 import okhttp3.*
-import okio.utf8Size
 import org.json.JSONException
 import org.json.JSONObject
 import java.io.IOException
@@ -17,13 +19,15 @@ class MainActivity : AppCompatActivity() {
     private val client = OkHttpClient()
     var isCorrect = false
     var correctAnswer = ""
-    var questionNum = 0
+    var questionNum = 15
+    var score = 0
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+        var scoreTextView = findViewById<TextView>(R.id.scoreTextView)
 
         fetchData()
     }
@@ -76,23 +80,51 @@ class MainActivity : AppCompatActivity() {
 
                 if (response.isSuccessful) runOnUiThread {
                     questionNum++
-                    questionCounterTextView.findViewById<TextView>(R.id.questionCounterTextView)
-                    questionCounterTextView.text = "Question: $questionNum of 20"
-                    questionTextView.text = question
-                    answerList.add(correctAnswer)
-                    answerList.shuffle()
-                    var answerListView = findViewById<ListView>(R.id.answerListView)
-                    var myAdapter = ArrayAdapter<String> (this@MainActivity, android.R.layout.simple_list_item_1, answerList)
-                    answerListView.adapter = myAdapter
-                    answerListView.onItemClickListener = AdapterView.OnItemClickListener { parent, view, position, id ->
-                        var selectedAnswer = answerListView.getItemAtPosition(position)
-                        isCorrect = selectedAnswer.equals(correctAnswer)
-                        showResultDialog()
+                    if(questionNum > 20){
+                        gameOver()
+                    } else {
+                        questionCounterTextView.findViewById<TextView>(R.id.questionCounterTextView)
+                        questionCounterTextView.text = "Question: $questionNum of 20"
+                        questionTextView.text = question
+                        answerList.add(correctAnswer)
+                        answerList.shuffle()
+                        var answerListView = findViewById<ListView>(R.id.answerListView)
+                        var myAdapter = ArrayAdapter<String> (this@MainActivity, android.R.layout.simple_list_item_1, answerList)
+                        answerListView.adapter = myAdapter
+                        answerListView.onItemClickListener = AdapterView.OnItemClickListener { parent, view, position, id ->
+                            var selectedAnswer = answerListView.getItemAtPosition(position)
+                            isCorrect = selectedAnswer.equals(correctAnswer)
+                            showResultDialog()
 
+                        }
                     }
+
                 }
             }
         })
+
+    }
+
+    private fun gameOver() {
+        var percentage = ((score / 20)*100)
+        val goDialog = AlertDialog.Builder(this)
+        goDialog.setTitle("Congratulations!")
+        goDialog.setMessage("Your score is: $percentage%")
+        goDialog.setCancelable(true)
+        goDialog.setPositiveButton("Restart") {dialog, which ->
+
+            resetGame()
+
+        }
+        goDialog.show()
+
+    }
+
+    private fun resetGame() {
+        questionNum = 0
+        score = 0
+        fetchData()
+        scoreTextView.text = "Score: 0"
 
     }
 
@@ -101,6 +133,10 @@ class MainActivity : AppCompatActivity() {
         if(isCorrect){
             dialog.setTitle("$correctAnswer is Correct")
             dialog.setMessage("Good Job!")
+            score++
+
+            scoreTextView.text = "Correct: $score"
+
         } else {
             dialog.setTitle("Incorrect")
             dialog.setMessage("The correct answer is $correctAnswer")
@@ -115,9 +151,5 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    private fun cleanText(text : String): String {
-        text.replace("&quot;" , "'" )
-        return text
 
-    }
 }
